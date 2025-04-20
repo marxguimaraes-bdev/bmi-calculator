59df59ab55643a7499df5768130e9303132208ba
Marx Guimarães <marx.guimaraes@bairesdev.com>
2025-04-20 15:54:41 -0400

    fix: fine tune vertical gaps in a few elements

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import SmallCard from "@/components/small-card";
 export default function IndexPage() {
   return (
     <div class="bg-pure-white">
-      <main className='flex flex-col font-custom items-center bg-pure-white gap-y-10 xl:max-w-[90rem] xl:mx-auto'>
+      <main className='flex flex-col font-custom items-center bg-pure-white xl:max-w-[90rem] xl:mx-auto pb-[96px] xl:pb-[120px]'>
         <section title="Body Mass Index Calculator" className="flex flex-col w-full xl:overflow-visible xl:flex-row xl:px-6">
           <div className="flex flex-col items-center gap-y-[1.5rem] sm:gap-y-[2.5rem] xl:gap-y-[7.9375rem] text-center rounded-b-heading p-6 min-h-[40rem] bg-gradient-linear xl:py-[4.47625rem] sm:px-22 xl:pl-[7.25rem] xl:pr-[18.625rem] xl:max-w-[61.125rem] xl:h-[46.0625rem] xl:items-start xl:text-left">
             <Image src="/images/logo.svg" width={40} height={40} alt="Company logo" className="xl:w-[4rem] xl:h-[4rem]" />
@@ -17,7 +17,10 @@ export default function IndexPage() {
           </div>
           <Calculator />
         </section>
-        <section title="What your BMI result means" className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 sm:mx-[2.6875rem] xl:mx-[8.75rem] gap-y-8 xl:gap-y-[2.1875rem] xl:mt-[1.5625rem]">
+        <section title="What your BMI result means" className={`
+          grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 w-full sm:px-[2.6875rem] xl:px-[8.75rem] gap-y-8 xl:gap-y-[2.1875rem]
+          mt-[72px] sm:mt-[96px] xl:mt-[65px]
+        `}>
           <Image src="/images/image-man-eating.webp" alt="Man eating sushi" width={564} height={533} className="mb-4 sm:mb-0 sm:row-span-3 sm:-ml-[9.9375rem] xl:ml-0 sm:min-w-[27.1875rem] xl:w-[35.25rem] xl:h-[33.3125rem] xl:self-end" />
           <Image src="/images/pattern-curved-line-left.svg" alt="Line curved to the left" width={85} height={200} className="hidden xl:flex justify-self-end self-start absolute xl:mr-[1.6875rem] xl:-mt-[3.25rem]" />
           <h1 className="px-6 sm:px-0 sm:mt-[2.1875rem] xl:mt-[11.5rem] xl:w-[29.0625rem] xl:justify-self-end text-heading text-gunmetal leading-heading font-semibold leading-heading-s xl:text-heading-l xl:tracking-tighter">What your BMI result means</h1>
@@ -25,10 +28,10 @@ export default function IndexPage() {
             A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.
           </div>
         </section>
-        <section title="health tips" className={`
+        <section title="Health tips" className={`
           grid gap-y-10 gap-x-6 xl:grid-cols-3 bg-gradient-linear
-          my-[72px] py-[56px] pl-[19px] pr-[29px]
-          sm:my-24 sm:py-[60px] sm:pl-[39px] sm:pr-[43px]
+          mt-[72px] py-[56px] pl-[19px] pr-[29px]
+          sm:mt-[96px] sm:py-[60px] sm:pl-[39px] sm:pr-[43px]
           xl:mx-6 xl:px-[116px] xl:pt-[72px] xl:pb-[96px] xl:rounded-[35px]
         `}>
           <Card icon="/images/icon-eating.svg" alt="Food icon" title="Healthy eating">
@@ -43,9 +46,9 @@ export default function IndexPage() {
         </section>
         <section title="Limitations of BMI" className={`
           grid gap-y-4 grid-cols-1 sm:grid-cols-2 sm:gap-x-5 xl:grid-cols-3 xl:gap-6 w-full
-          pt-8 pb-24 pl-[19px] pr-[29px]
-          sm:pt-14 sm:pl-[39px] sm:pr-[43px]
-          xl:pt-6 xl:pb-[120px] xl:pr-0 xl:pl-0
+          mt-[72px] pl-[19px] pr-[29px]
+          sm:mt-24 sm:pl-[39px] sm:pr-[43px]
+          xl:mt-[120px] xl:pr-0 xl:pl-0
         `}>
           <div className="flex flex-col text-center xl:text-left gap-y-8 pb-10 sm:col-span-2 xl:max-w-[564px] xl:ml-[140px]">
             <h1 className="text-heading text-gunmetal leading-heading font-semibold leading-heading-s xl:text-heading-l">Limitations of BMI</h1>
