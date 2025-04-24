d355b61aaa5429c5fd02c0bc0a0f308057970926
Marx Guimarães <marx.guimaraes@bairesdev.com>
2025-04-23 21:12:26 -0400

    fix: adjust card component spacings

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 function Card({ icon, alt, title, children }) {
   return (
-    <div className="flex flex-col gap-y-4 md:flex-row lg:flex-col">
-      { icon && <Image src={icon} alt={alt} width={64} height={64} />}
-      <div className="flex flex-col gap-y-6 md:mx-10 md:gap-y-8 lg:mx-0">
-        <h2 className="text-heading-m text-gunmetal leading-heading-m font-semibold md:col-span-4">{title}</h2>
+    <div className="flex flex-col gap-y-8 sm:gap-x-10 xl:gap-y-[2.8125rem] sm:flex-row xl:flex-col">
+      {icon && <Image src={icon} alt={alt} width={64} height={64} />}
+      <div className="flex flex-col gap-y-6">
+        <h2 className="text-heading-m text-gunmetal leading-heading-m font-semibold sm:col-span-4">{title}</h2>
         <div className="text-body text-dark-electric-blue">{children}</div>
       </div>
     </div>
